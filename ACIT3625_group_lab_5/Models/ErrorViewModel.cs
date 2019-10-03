c58a8704890b6824333c864417d7fb5bffb33611
Yuyu Lai <yuyulai97@gmail.com>
2019-10-03 09:50:27 -0700

    Add project files.

---
+++ ACIT3625_group_lab_5/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ACIT3625_group_lab_5.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
